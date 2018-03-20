43babc658757cd06c2060b67a41804cad9e89776
Greg Stimson <stimsong@yahoo.com>
2018-03-20 17:16:05 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,14 +3,14 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       :patriarch => {},
-      :martiarch => {},
+      :matiarch => {},
       :hero => {},
       :hero_friends => {}
       },
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
-      :martiarch => {},
+      :matiarch => {},
       :hero => {},
       :hero_friends => {}
       }
